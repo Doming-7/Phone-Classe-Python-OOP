7c6a6377ea11a4a7cba15109f39c40118cdd4977
Doming <doming@192.168.11.127>
2026-04-23 23:49:15 +0100

    Phone Exercice Python Poo

---
+++ test.py
@@ -0,0 +1,24 @@
+class Phone:
+    def __init__(self,brand,model,storage,inStock):
+        self.brand = brand
+        self.model = model
+        self.storage = storage
+        self.inStock = inStock
+
+    def Afficher(self):
+        if self.inStock == True:
+            currently = "in stock"
+        else:
+            currently = "out of stock"
+        print(f"{self.model} by {self.brand} has {self.storage}GB of storage and is currently {currently}")
+
+p1 = Phone("Samsung","Galaxy S23",128,True)
+p2 = Phone("Apple","Iphone 15 Pro",256,False)
+p3 = Phone("Xiaomi","Redmi Note 12",64,True)
+
+print("====Phone1====")
+p1.Afficher()
+print("====Phone2====")
+p2.Afficher()
+print("====Phone3====")
+p3.Afficher()
